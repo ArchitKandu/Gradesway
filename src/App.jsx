e9ec8d98101a66260b37a0a9895d46a0c236501d
Archit Kandu <architkandu@hotmail.com>
2025-02-24 00:44:06 +0545

    Created Login functionality, successful only if dummy credentials used

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+import FormPage from './components/FormPage';
 
 function App() {
 
   return (
-    <h1>
-      Hello, World!
-    </h1>
-  )
-}
+    <Router>
+      <Routes>
+        <Route 
+          path="/" 
+          element={
+            // if user has already logged in current session redirect to form page
+            localStorage.getItem("loggedIn") === "true" ? <Navigate to="/form" replace /> : <Login />
+          } 
+        />
+        <Route path = "/form" element = {
+          // if only user has logged in display form page else redirect to home
+           localStorage.getItem("loggedIn") === "true" ? <FormPage /> : <Navigate to="/" replace />
+        } 
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/FormPage.jsx
@@ -0,0 +1,11 @@
+const FormPage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
+                <h2 className="text-2xl font-bold text-gray-900 text-center">Welcome to the Form Page</h2>
+            </div>
+        </div>
+    );
+};
+
+export default FormPage;
