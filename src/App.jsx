14b8453e498b43a507d5729f45e6065d3f2aec19
Archit Kandu <architkandu@hotmail.com>
2025-02-24 13:36:48 +0545

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import FormPage from './components/FormPage';
+import './App.css'
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            // if user has already logged in current session redirect to form page
-            localStorage.getItem("loggedIn") === "true" ? <Navigate to="/form" replace /> : <Login />
-          } 
-        />
-        <Route path = "/form" element = {
-          // if only user has logged in display form page else redirect to home
-           localStorage.getItem("loggedIn") === "true" ? <FormPage /> : <Navigate to="/" replace />
-        } 
-        />
-      </Routes>
-    </Router>
-  );
-};
+    <>
+     <h1>Hello, World !</h1>
+    </>
+  )
+}
 
-export default App;
+export default App
